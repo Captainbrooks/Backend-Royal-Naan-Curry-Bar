2fb3f730852fa1ffef93aeb4e707f647b9b51186
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-05-29 14:32:45 -0400

    Search Api Made
    Added a Search Api based on names of the foods

--- Routes/menuRoutes.js
+++ Routes/menuRoutes.js
@@ -2,15 +2,16 @@ const express=require("express");
 const router=express.Router();
 const requireAuth=require("../Authentication/requireAuth");
 const isAdmin=require("../Authentication/isAdmin");
-const {addFoodItem, getFoodByCategory,getMainCoursesBySubCategory,getDrinksBySubCategory,getAllFoods, deleteFood}=require("../Controllers/menuController");
+const {addFoodItem, getFoodByCategory,getMainCoursesBySubCategory,getDrinksBySubCategory,getAllFoods, deleteFood, SearchFood}=require("../Controllers/menuController");
 
 router.get("/fooditem/getfoods",getAllFoods);
 router.get("/fooditem/:category",getFoodByCategory)
 router.get("/fooditem/MainCourses/:subcategory",getMainCoursesBySubCategory)
 router.get("/fooditem/DrinkMenus/:subcategory",getDrinksBySubCategory)
-router.post("/fooditem",addFoodItem);
+router.post("/fooditem/search",addFoodItem);
+
+router.get("/fooditem/getfoods/search",SearchFood)
 
 router.delete("/fooditem/delete/:id",isAdmin,deleteFood)
 
 module.exports= router;
-
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,21 +1,23 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
 
-const {SignUp,Login,bookReservation,forgotPassword,verifyPasswordLink,resetPassword}=require("../Controllers/userController");
+const { SignUp, Login, bookReservation, forgotPassword, verifyPasswordLink, resetPassword } = require("../Controllers/userController");
 
 
 
-router.post("/user/signup",SignUp);
-router.post("/user/login",Login);
-router.post("/reservations/add-reservations",bookReservation);
+router.post("/user/signup", SignUp);
+router.post("/user/login", Login);
+router.post("/reservations/add-reservations", bookReservation);
 
-router.post("/user/forgot-password",forgotPassword)
 
-router.get("/user/reset-password",verifyPasswordLink);
-router.post("/user/reset-password",resetPassword);
+router.post("/user/forgot-password", forgotPassword)
 
-module.exports=router;
+router.get("/user/reset-password", verifyPasswordLink);
+router.post("/user/reset-password", resetPassword);
+
+
+module.exports = router;
 
 
 
