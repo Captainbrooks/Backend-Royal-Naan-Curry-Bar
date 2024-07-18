8efb436be2bccc5bf1a91209fbc57c88a1a34510
Captainbrooks <138337847+Captainbrooks@users.noreply.github.com>
2024-07-18 16:35:33 -0300

    w
    s

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
 
-const app=express();
-const cors=require("cors");
+const app = express();
+const cors = require("cors");
 app.use(express.json({
-    limit: '50mb'
-  }));
+  limit: '50mb'
+}));
+
+
 app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '50mb' }));
 const cookieParser = require('cookie-parser');
 
 
+const connectDb = require("./dbConnection/dbConnection");
+const menuRoutes = require("./Routes/menuRoutes")
+const userRoutes = require("./Routes/userRoutes");
+const messageRoutes = require("./Routes/messageRoutes");
+const adminRoutes = require("./Routes/adminRoutes");
 
-
-
-
-
-
-const connectDb=require("./dbConnection/dbConnection");
-const menuRoutes=require("./Routes/menuRoutes")
-const userRoutes=require("./Routes/userRoutes");
-const messageRoutes=require("./Routes/messageRoutes");
-const adminRoutes=require("./Routes/adminRoutes");
-
-app.use("/api/v1",adminRoutes);
-app.use("/api/v1",menuRoutes);
-app.use("/api/v1",userRoutes);
-app.use("/api/v1",messageRoutes);
+app.use("/api/v1", adminRoutes);
+app.use("/api/v1", menuRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", messageRoutes);
 
 
 
 
 connectDb();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Hello, backend is working properly and it is fetchhing the info")
 })
 
 
-
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running at ${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`)
 })
